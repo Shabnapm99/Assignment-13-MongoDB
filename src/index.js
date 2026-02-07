c520063b59951912e7870f020295ac7dbae8861c
Shabna P M <pmshabnashareefa@gmail.com>
2026-02-07 23:27:32 +0300

    implemented db connection and created Schema and model

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import postRoute from './routes/postRoutes.js';
+import { dbConnect } from './config/db.js';
 
 const app = express();
 const PORT = 3000;
 
 app.use('/',postRoute);
-
+dbConnect();
 app.listen(PORT,()=>console.log(`App is running on port ${PORT}`));
 
--- src/model/postModel.js
+++ src/model/postModel.js
@@ -1,5 +1,20 @@
 import {Schema} from 'mongoose';
 
 const postSchema = new Schema({
+title:{
+    type:String,
+    minLength:3,
+    trim:true,
+    required:true
+},
+content:{
+    type:String,
+    minLength:3,
+    trim:true,
+    required:true
+}
+});
 
-})
+const PostModel = mongoose.model('Post',postSchema);
+
+export default PostModel;
